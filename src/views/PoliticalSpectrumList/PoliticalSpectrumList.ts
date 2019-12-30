f094419404286a8f4c5cf80a3f19a31e27b57929
Avik <aviknandi@yahoo.com>
2019-12-30 22:54:39 +0530

    initial commit

---
+++ src/views/PoliticalSpectrumList/PoliticalSpectrumList.ts
@@ -0,0 +1,174 @@
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import axios from 'axios';
+
+@Component({
+  components: {
+  }
+})
+export default class PoliticalSpectrumList extends Vue {
+  dialog= false;
+  dialog2= false;
+  loading= false;
+  valid= true;
+  editIndex = 0;
+  editData:any = ""
+  baseUrl = "";
+  token = "";
+  inject = ['theme'];
+
+  componentLoader = false;
+  mainComponent = false;
+
+  table = false;
+
+  /*==== input fields variables ======*/
+  election = "";
+  election2 = "";
+  spectrum = "";
+  min = "";
+  max = "";
+  issue = "";
+
+  /*==== dropdown variables ======*/
+  items= [];
+  items2= [];
+
+  /*==== data table varibles ======*/
+  headers = [
+    { text: 'Id', value: 'id' },
+    { text: 'Political Spectrum Name', value: 'spectrum' },
+    { text: 'Min', value: 'lowText' },
+    { text: 'Max', value: 'highText' },
+    { text: 'Issue', value: 'issueName' },
+    // { text: 'Election Name', value: 'electionName' },
+    // { text: 'Election Id', value: 'electionId', align: ' d-none' },
+    // { text: 'Active', value: 'active', align: ' d-none' },
+    { text: 'Actions', value: 'action', sortable: false }
+  ]
+  data = [ ];
+
+  /*======= operations ======*/
+  async created(){
+    this.componentLoader = true;
+    this.mainComponent = false;
+    this.baseUrl = this.$store.state.baseUrl;
+    this.token = this.$store.state.token;
+    //geting all distric list
+    const districData = await axios.post(this.baseUrl+"get-all-election-list", {"token": this.token});
+    console.log(districData.data.data)
+    const newDistData = districData.data.data.map((el:any)=>{
+      return {
+        id: el.id,
+        name: el.name
+      }
+    })
+    this.items = newDistData;
+    this.items2 = newDistData;
+    this.componentLoader = false;
+    this.mainComponent = true;
+  }
+
+  async submit(){
+    this.loading = true;
+    if(this.editIndex === 0){
+      const data = {                       // New Insert
+        "token": this.token,
+        "name" : this.spectrum,
+        "electionId": this.election,
+        "minName": this.min,
+        "maxName": this.max,
+        "issue": this.issue
+      }
+      console.log(data)
+      const resData = await axios.post(this.baseUrl+"add-edit-political-spectrum",data);
+      const res = await axios.post(this.baseUrl+"get-political-spectrum-list",{
+        token: this.token,
+        electionId: this.election
+      })
+      this.data = res.data.data
+      this.election2 = this.election
+    }else{                                // Edits Records
+      console.log(typeof(this.election))
+      if(typeof(this.election) === "number"){
+        const data = {
+          "token": this.token,
+          "name" : this.spectrum,
+          "electionId": this.election,
+          "minName": this.min,
+          "maxName": this.max,
+          "issue": this.issue,
+          "id": this.editData.id,
+          "active": true        
+        }
+        console.log(data);
+        const resData = await axios.post(this.baseUrl+"add-edit-political-spectrum",data);
+        const res = await axios.post(this.baseUrl+"get-political-spectrum-list",{
+          token: this.token,
+          electionId: this.election
+        })
+        this.data = res.data.data;
+        this.election2 = this.election;
+      }else{
+        const data = {
+          "token": this.token,
+          "name" : this.spectrum,
+          // @ts-ignore
+          "electionId": this.election.id,
+          "minName": this.min,
+          "maxName": this.max,
+          "issue": this.issue,
+          "id": this.editData.id,
+          "active": true        
+        }
+        console.log(data);
+        const resData = await axios.post(this.baseUrl+"add-edit-political-spectrum",data);
+        const res = await axios.post(this.baseUrl+"get-political-spectrum-list",{
+          token: this.token,
+          // @ts-ignore
+          electionId: this.election.id
+        })
+        this.data = res.data.data;
+        // @ts-ignore
+        this.election2 = this.election.id
+      }      
+    }
+    this.loading = false;
+    this.dialog = false;
+    this.dialog2 = false;
+    // @ts-ignore
+    this.$refs.form.reset();
+    this.editIndex = 0
+  }
+
+  /* ===== choosing item for edit =====*/
+  async editItem(item:any){
+    this.spectrum = item.spectrum;
+    // @ts-ignore
+    this.election = {name:item.electionName,id:this.election2}
+    this.min = item.lowText;
+    this.max = item.highText;
+    this.issue = item.issueName;
+    this.dialog = true;
+    this.editIndex =1;
+    this.editData = item
+    console.log(this.editData);
+  }
+
+  newItem(){
+    // @ts-ignore
+    this.$refs.form.reset();
+    this.editIndex = 0;
+  }
+
+  async selectElection(item:any){
+    console.log(item)
+    const data = {
+      token: this.token,
+      electionId: item
+    }
+    const res = await axios.post(this.baseUrl+"get-political-spectrum-list",data)
+    this.data = res.data.data
+    this.table = true;
+  }
+}
---
+++ src/views/PoliticalSpectrumList/PoliticalSpectrumList.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-container fluid>
+    <v-card class="text-center" v-if="componentLoader" style="height:85vh;padding-top:32.5vh">
+      <v-progress-circular
+      :size="50"
+      color="primary"
+      indeterminate
+      ></v-progress-circular>
+    </v-card>
+    <v-row v-if="mainComponent">
+      <v-col cols="6">
+        <v-select
+          v-model="election2"
+          :items="items2"
+          item-text="name"
+          item-value="id"
+          label="Select Election"
+          outlined
+          @input = "selectElection(election2);"
+        ></v-select>
+      </v-col>
+    <v-col cols="6">
+      <v-dialog v-model="dialog2" width="500" >
+        <template v-slot:activator="{ on }">
+          <v-btn color="red lighten-2" dark v-on="on" @click="newItem()">
+            Add New
+          </v-btn>
+        </template>
+
+        <v-card style="padding:20px">
+          <v-row>
+            <v-col cols="12">
+              <v-form  v-model="valid" ref="form">
+                <v-select
+                  v-model="election"
+                  :items="items"
+                  item-text="name"
+                  item-value="id"
+                  label="Select Election"
+                  :rules="[v => !!v || 'Item is required']"
+                  outlined
+                ></v-select>
+
+                <v-text-field
+                  v-model="spectrum"
+                  label="Spectrum Name"
+                  :rules="[v => !!v || 'Item is required']"
+                  outlined
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="min"
+                  label="Min Limit"
+                  :rules="[v => !!v || 'Item is required']"
+                  outlined
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="max"
+                  label="Max Limit"
+                  :rules="[v => !!v || 'Item is required']"
+                  outlined
+                ></v-text-field>
+
+                <v-text-field
+                  v-model="issue"
+                  label="Issue"
+                  :rules="[v => !!v || 'Item is required']"
+                  outlined
+                ></v-text-field>
+              </v-form>
+              <v-btn color="blue darken-1" dark @click="submit()" :disabled="!valid" :loading="loading">Save</v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-dialog>
+    </v-col>  
+    
+      <v-col cols="12" v-if="table">
+        <v-data-table :headers="headers" :items="data" class="elevation-1" >
+
+        <template v-slot:top>
+          <v-toolbar flat color="white">
+            <v-dialog v-model="dialog" max-width="500px">
+              <!-- <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark class="mb-2" v-on="on" @click="newItem()">New Item</v-btn>
+              </template> -->
+              <v-card>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-form  v-model="valid" ref="form">
+                          <v-select
+                            v-model="election"
+                            :items="items"
+                            item-text="name"
+                            item-value="id"
+                            label="Select Election"
+                            :rules="[v => !!v || 'Item is required']"
+                            outlined
+                          ></v-select>
+
+                          <v-text-field
+                            v-model="spectrum"
+                            label="Spectrum Name"
+                            :rules="[v => !!v || 'Item is required']"
+                            outlined
+                          ></v-text-field>
+
+                          <v-text-field
+                            v-model="min"
+                            label="Min Limit"
+                            :rules="[v => !!v || 'Item is required']"
+                            outlined
+                          ></v-text-field>
+
+                          <v-text-field
+                            v-model="max"
+                            label="Max Limit"
+                            :rules="[v => !!v || 'Item is required']"
+                            outlined
+                          ></v-text-field>
+
+                          <v-text-field
+                            v-model="issue"
+                            label="Issue"
+                            :rules="[v => !!v || 'Item is required']"
+                            outlined
+                          ></v-text-field>
+                        </v-form>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <!-- <v-btn color="blue darken-1" text @click="close">Cancel</v-btn> -->
+                  <v-btn color="blue darken-1" dark @click="submit()" :disabled="!valid" :loading="loading">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+
+        <template v-slot:item.action="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <!-- <v-icon small @click="deleteItem(item)">
+            mdi-delete
+          </v-icon> -->
+        </template>
+
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+<script lang="ts" src = './PoliticalSpectrumList.ts'>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
